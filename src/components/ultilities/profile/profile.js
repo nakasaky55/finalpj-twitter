65fbc289369131df96df097d7d1792990d32a8c5
Nakasaky55 <themaskmen1996@gmail.com>
2019-12-17 02:14:45 +0700

    img resize

--- src/components/ultilities/profile/profile.js
+++ src/components/ultilities/profile/profile.js
@@ -160,7 +160,7 @@ export default function Profile(props) {
             cloudName="hslqp9lo2"
             publicId={avaUrl}
             responsive
-            style={{maxWidth:"100%"}}
+            style={{ width: "100%", height: "100%" }}
           >
             <Transformation gravity="face" crop="scale" />
           </Image>
